e16b50c856fedc81042d82a66da00c305b3cf8f7
Your Name <you@example.com>
2021-12-01 14:39:42 +0100

    lab7

--- classes/rating.py
+++
@@ -1,6 +0,0 @@
-class Ratings:
-    def __init__(self, userID: int, movieID: int, rating: float, timestamp: int):
-        self.userID = userID
-        self.movieID = movieID
-        self.rating = rating
-        self.timestamp = timestamp
--- services/read_file.py
+++
@@ -1,5 +0,0 @@
-def read_file(path: str):
-    f = open(path, encoding='UTF-8')
-    data = f.read().splitlines()
-    f.close()
-    return data
